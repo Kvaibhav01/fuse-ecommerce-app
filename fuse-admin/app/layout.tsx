0634ce64eaeac784f82541816cef0a29e4f1c0f7
Vaibhav <khulbevaibhavdev@gmail.com>
2023-07-18 19:24:33 +0530

    Initial project setup w/ Next.js + shadcn/ui

---
+++ fuse-admin/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fuse ⋯ Admin Dashboard",
+  description: "The admin dashboard of the Fuse e-commerce app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
