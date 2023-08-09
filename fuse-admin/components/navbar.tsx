a2bc444384188d7b3c6c92428ff0b9315ed799b8
Vaibhav <khulbevaibhavdev@gmail.com>
2023-08-09 17:50:58 +0530

    Added temperory logo

--- fuse-admin/components/navbar.tsx
+++ fuse-admin/components/navbar.tsx
@@ -3,6 +3,10 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import Image from "next/image";
+
+import logo from "../images/logo.svg";
+import Link from "next/link";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -20,10 +24,21 @@ const Navbar = async () => {
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores} />
+      {/* TODO: Justify space evently or update the left-right padding to be narrower. Then update padding for the body as well */}
+      <div className="flex h-16 items-center justify-between px-4">
+        <div className="flex items-center space-x-8">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="Logo of the Fuse app"
+              width={80}
+              height={80}
+            />
+          </Link>
+          <StoreSwitcher items={stores} />
+        </div>
         <MainNav className="mx-6" />
-        <div className="ml-auto  flex items-center space-x-4">
+        <div className="flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
